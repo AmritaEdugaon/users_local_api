5f9ac2062d8a6a2931926768f76d03f2be2bd8ef
AmritaEdugaon <amritaedugaon281124@gmail.com>
2025-11-04 16:22:26 +0530

    i changed some codes in this apis.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const users=[
     {"id":1,"name":"amrit","email":"amrit1234@kumarigmail.com"},
     {"id":2,"name":"pooja","email":"pooja834@kumarigmail.com"},
     {"id":3,"name":"nagma","email":"nagma134@kumarigmail.com"},
-    {"id":4,"name":"hena","email":"hena7234@kumarigmail.com"},
+    {"id":4,"name":"hena","email":"hena7234@kumarigmail.com"}
 ];
 app.get("/users",(request,response)=>{
     response.status(200).json(users);
@@ -61,5 +61,5 @@ app.delete("/deleteUser/:id",(request,response)=>{
 
 app.listen(PORT,(error)=>{
     if(error) throw error;
-    console.log("server is runing on http://localhost:/4000")
+    console.log('server is runing on http://localhost:/${PORT}')
 });
